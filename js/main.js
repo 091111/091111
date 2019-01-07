5cccb95afb562e7fd225715eae699c3eab6a1f1b
091111 <34302881+091111@users.noreply.github.com>
2019-01-07 16:34:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function getToken(){
 
 	$.ajax("https://apis.jins.com/meme/v1/oauth/token", {
 		type: "POST",
+		dataType: "jsonp",
 		headers: { "Content-Type": "application/x-www-form-urlencoded" },
 		body: { 
 			"grant_type": "authorization_code",
