421561e0c86ecbb800e73a30955bdc5fc1cc4c16
091111 <091111@users.noreply.github.com>
2019-01-07 15:48:28 +0900

    First commit

---
+++ js/login.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("#btnLogin").click(memelogin);
+});
+
+function memelogin(){
+	window.open('https://accounts.jins.com/jp/ja/oauth/authorize?response_type=code&client_id=793384252606287&redirect_uri=http://tipporn.com/main.html&state=jshcu5tqhs&scope=office&service_id=meme&login_hint=nb15505@shibaura-it.ac.jp');
+} 
---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	getToken();
+});
+
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
+
+function getToken(){
+	var urlcode = getUrlVars()["code"];
+	alert(urlcode);
+
+	$.ajax("https://apis.jins.com/meme/v1/oauth/token", {
+		type: "POST",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body: { 
+			"grant_type": "authorization_code",
+			"code": urlcode,
+			"redirect_uri": "http://tipporn.com/main.html",
+			"client_id": "793384252606287",
+			"client_secret": "uobinmueqiohlkcvyxby3unmyli4u1wa"
+		},
+		success: function(){
+			alert("success");
+		},
+		error: function(){
+			alert("error");
+		}
+	});
+}
+
+/*function getTokenTest() {
+	var urlcode = getUrlVars()["code"];
+	alert(urlcode);
+
+	var request = new XMLHttpRequest();
+	var params = 'grant_type: authorization_code, code:' + urlcode + ', redirect_uri: http://tipporn.com/main.html, client_id: 793384252606287, client_secret: uobinmueqiohlkcvyxby3unmyli4u1wa';
+	request.open('POST', 'https://apis.jins.com/meme/v1/oauth/token', true);
+	request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	request.onreadystatechange = function() {
+		if(request.readyState == 4 && request.status == 200) {
+			alert("ok");
+		}
+	}
+	request.send(params);
+}*/
