b005e57d40546456c7301b5b449000425ddd74b6
091111 <34302881+091111@users.noreply.github.com>
2019-01-07 16:40:43 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,5 +3,5 @@ $(document).ready(function(){
 });
 
 function memelogin(){
-	window.open('https://accounts.jins.com/jp/ja/oauth/authorize?response_type=code&client_id=793384252606287&redirect_uri=https://091111.github.io/main.html&state=jshcu5tqhs&scope=office&service_id=meme&login_hint=nb15505@shibaura-it.ac.jp');
+	window.open('https://accounts.jins.com/jp/ja/oauth/authorize?response_type=code&client_id=793384252606287&redirect_uri=https://091111.github.io/index.html&state=jshcu5tqhs&scope=office&service_id=meme&login_hint=nb15505@shibaura-it.ac.jp');
 } 
